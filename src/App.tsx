cf8f6869cb8d0fb6e234136aa13f40d9dd920b53
Sriyanshjain <shreyanshjain036@gmail.com>
2021-09-21 16:53:44 +0530

    initial code

--- src/App.tsx
+++ src/App.tsx
@@ -3,48 +3,24 @@ import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import './App.css';
+import '../src/loginpage';
+import Loginpage from '../src/loginpage';
 
 function App() {
   return (
 
-    <div className="maincontainer">
-    <div className="container-fluid">
-        <div className="row no-gutter">
-           
-            <div className="col-md-6 d-none d-md-flex bg-image"></div>
-            
-            <div className="col-md-6 bg-light">
-                <div className="login d-flex align-items-center py-5">
-                   
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-lg-10 col-xl-7 mx-auto">
-                                <h3 className="display-4">Split login page!</h3>
-                                <p className="text-muted mb-4">Create a login split page using Reactjs & Bootstrap 5.</p>
-                                <form>
-                                    <div className="mb-3">
-                                        <input id="inputEmail" type="email" placeholder="Email address" required={true}  className="form-control rounded-pill border-0 shadow-sm px-4" />
-                                    </div>
-                                    <div className="mb-3">
-                                        <input id="inputPassword" type="password" placeholder="Password" required={true} className="form-control rounded-pill border-0 shadow-sm px-4 text-primary" />
-                                    </div>
-                                    <div className="form-check">
-                                        <input id="customCheck1" type="checkbox" checked className="form-check-input" />
-                                        <label htmlFor="customCheck1" className="form-check-label">Remember password</label>
-                                    </div>
-                                    <div className="d-grid gap-2 mt-2">
-                                    <button type="submit" className="btn btn-primary btn-block text-uppercase mb-2 rounded-pill shadow-sm">Sign in</button>
-                                    </div>
-                                    
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+    <body >
+    <div className="container-fluid mycontainer" >
+       <div className="row">
+            <div className="col formdiv">
+              <Loginpage></Loginpage>
             </div>
+            <div className="col imagediv">
+             
         </div>
-    </div>
-  </div>
+        </div>
+    </div>   
+    </body> 
   );
 }
 
---
+++ src/loginpage.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './App.css';
+function Loginpage(){
+   return(
+    <h3 className="account_text">Don't have an account?<span className="signuplink"> Sign up</span></h3>
+   ) 
+}
+export default Loginpage;
