56d7bd1269beb21383615227cd5f8c3a3d9429be
Sriyanshjain <shreyanshjain036@gmail.com>
2021-09-23 23:55:17 +0530

    cleanup

--- src/vantaclass.js
+++ src/vantaclass.js
@@ -1,56 +1,51 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import TRUNK from '../node_modules/vanta/dist/vanta.trunk.min';
 import p5Min from './p5.min.js';
 import newBanner from '../src/newBanner.png';
-// const s:any=require('../node_modules/vanta/dist/vanta.trunk.min.js');
 
-const elem = (document.compatMode === "CSS1Compat") ? 
-    document.documentElement :
-    document.body;
+const elem = (document.compatMode === "CSS1Compat") ?
+  document.documentElement :
+  document.body;
 
 var height = elem.clientHeight;
 export default class Vantaclass extends Component {
 
-    
-    constructor(props) {
-        super(props)
-    
-        this.vantaRef = React.createRef()
-    }
-    
-    componentDidMount() {
-      this.vantaEffect = TRUNK({
-        el: this.vantaRef.current,
-        mouseControls: true,
-  touchControls: true,
-  gyroControls: false,
-  minHeight: height,
-  
-  
-  scale: 1.00,
-  scaleMobile: 1.00,
-  color: '#3c3c79',
-  backgroundColor: '#030334',
-  spacing: 4.50,
-  chaos: 4.00,
-  p5:p5Min
-  
-      })
-    }
-   
-    
-    componentWillUnmount() {
-      if (this.vantaEffect) this.vantaEffect.destroy()
-    }
-    render() {
-      return <div className='vanta' ref={this.vantaRef}>
-          
-        <img src={newBanner} style={{height:'100vh'}}></img>
-      </div>
-    }
-  
+
+  constructor(props) {
+    super(props)
+    this.vantaRef = React.createRef()
   }
-  
- 
-  
+
+  componentDidMount() {
+    this.vantaEffect = TRUNK({
+      el: this.vantaRef.current,
+      mouseControls: true,
+      touchControls: true,
+      gyroControls: false,
+      minHeight: height,
+      scale: 1.00,
+      scaleMobile: 1.00,
+      color: '#3c3c79',
+      backgroundColor: '#030334',
+      spacing: 4.50,
+      chaos: 4.00,
+      p5: p5Min
+
+    })
+  }
+
+
+  componentWillUnmount() {
+    if (this.vantaEffect) this.vantaEffect.destroy()
+  }
+  render() {
+    return <div className='vanta' ref={this.vantaRef}>
+
+      <img src={newBanner} style={{ height: '100vh' }}></img>
+    </div>
+  }
+
+}
+
+
